db790ea828aa6f552dae53c0e08d24d394256eb7
Vincentius Christian Chandra <crist.vincent10@gmail.com>
2024-04-28 20:21:15 +0700

    Initial commit

---
+++ app/src/main/java/com/dev/firedetector/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dev.firedetector
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dev.firedetector.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/dev/firedetector/ui/emergency/EmergencyFragment.kt
@@ -0,0 +1,37 @@
+package com.dev.firedetector.ui.emergency
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.dev.firedetector.databinding.FragmentEmergencyBinding
+
+class EmergencyFragment : Fragment() {
+
+    private var _binding: FragmentEmergencyBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val emegencyViewModel =
+            ViewModelProvider(this)[EmegencyViewModel::class.java]
+
+        _binding = FragmentEmergencyBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
