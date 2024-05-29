b150d81d0edad734f0c39c62eae170fa6c2ddc68
Vincentius Christian Chandra <crist.vincent10@gmail.com>
2024-05-29 13:45:36 +0700

    permission to call success

--- app/src/main/java/com/dev/firedetector/ui/emergency/EmergencyFragment.kt
+++ app/src/main/java/com/dev/firedetector/ui/emergency/EmergencyFragment.kt
@@ -1,11 +1,19 @@
 package com.dev.firedetector.ui.emergency
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.dev.firedetector.databinding.FragmentEmergencyBinding
+
 
 class EmergencyFragment : Fragment() {
 
@@ -22,8 +30,46 @@ class EmergencyFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnPolisi.setOnClickListener {
+                val police = "tel:089605958454"
+                callPolice(police)
+            }
+            btnAmbulance.setOnClickListener {
+                val ambulance = "tel:0895600560600"
+                callPolice(ambulance)
+            }
+        }
+    }
+
+    private fun callPolice(number: String) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CALL_PHONE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse(number)))
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CALL_PHONE)
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(requireContext(), "Permission granted to make calls", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "Permission denied to make calls", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
